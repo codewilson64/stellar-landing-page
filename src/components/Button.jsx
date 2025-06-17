41cd2ad39689a5af6b7ca1a2a8c033c66e86ed55
codewilson64 <wilsongambit@gmail.com>
2025-06-17 19:28:17 +0700

    edit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,14 +3,17 @@ import appStoreIcon from '../assets/appstore.png'
 
 const Button = () => {
   return (
-    <div className='flex gap-3 w-full ss:w-[380px]'>
-        <button className='cursor-pointer'>
-          <img src={googlePlayIcon} alt="icon"/>
-        </button>
+    <div className='flex justify-center gap-3 w-full ss:w-[380px]'>
+      <button className='cursor-pointer'>
+        <img src={googlePlayIcon} alt="icon"/>
+      </button>
 
-        <button className='cursor-pointer'>
-          <img src={appStoreIcon} alt="icon" />
-        </button>
+        <button className='relative cursor-not-allowed'>
+          <img src={appStoreIcon} alt="App Store" className='opacity-30'/>
+          <span className='absolute w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-lg font-bold sm:text-lg text-black px-2 py-1 rounded'>
+            Coming Soon
+          </span>
+        </button>       
     </div>
   )
 }

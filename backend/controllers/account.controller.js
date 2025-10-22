0e085e3abb04b494b2fdba37487a74cbfa760854
codewilson64 <wilsongambit@gmail.com>
2025-10-22 15:49:19 +0700

    add account route

--- backend/controllers/account.controller.js
+++ backend/controllers/account.controller.js
@@ -11,13 +11,13 @@ export default async function handler(req, res) {
       return res.status(400).json({ success: false, message: "Missing userId" });
     }
 
-    const client = new Client()
-      .setEndpoint(process.env.APPWRITE_ENDPOINT)
-      .setProject(process.env.APPWRITE_PROJECT_ID)
-      .setKey(process.env.APPWRITE_API_KEY);
+    // const client = new Client()
+    //   .setEndpoint(process.env.APPWRITE_ENDPOINT)
+    //   .setProject(process.env.APPWRITE_PROJECT_ID)
+    //   .setKey(process.env.APPWRITE_API_KEY);
 
-    const users = new Users(client);
-    await users.delete(userId);
+    // const users = new Users(client);
+    // await users.delete(userId);
 
     return res.status(200).json({ success: true, message: "User deleted successfully" });
   } catch (error) {

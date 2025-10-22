af1c357d25ec5e9478208820b7e63c6a42c6f3ec
codewilson64 <wilsongambit@gmail.com>
2025-10-22 16:04:06 +0700

    add account route

--- backend/controllers/account.controller.js
+++ backend/controllers/account.controller.js
@@ -1,3 +1,5 @@
+import { Client, Users } from "node-appwrite"
+
 const deleteAccount = async (req, res) => {
   const { userId } = req.body
 
@@ -5,6 +7,14 @@ const deleteAccount = async (req, res) => {
     if (!userId) {
       return res.status(400).json({ success: false, message: "Missing userId" });
     }
+
+    const client = new Client()
+      .setEndpoint(process.env.APPWRITE_ENDPOINT)
+      .setProject(process.env.APPWRITE_PROJECT_ID)
+      .setKey(process.env.APPWRITE_API_KEY);
+
+    const users = new Users(client);
+    await users.delete(userId);
 
     return res.status(200).json({ success: true, message: "User deleted successfully" });
   } catch (error) {
